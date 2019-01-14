9d304fe3657897fad6e3edb218b4927d8bba57e6
Corne <39339251+corne12345@users.noreply.github.com>
2019-01-14 14:04:24 +0100

    update

---
+++ AppData/app/src/main/java/com/example/corne/sportbuddy/ViewActivity.java
@@ -0,0 +1,46 @@
+package com.example.corne.sportbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        TextView caloriesTextView = findViewById(R.id.textView9);
+        String caloriesString = (String) caloriesTextView.getText();
+        final int calories = Integer.valueOf(caloriesString);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(ViewActivity.this, InputActivity.class);
+                startActivity(intent1);
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(ViewActivity.this, InputActivity.class);
+                intent2.putExtra("calories", calories);
+                startActivity(intent2);
+            }
+        });
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent (ViewActivity.this, SportActivity.class);
+                intent3.putExtra("calories", calories);
+            }
+        });
+    }
+}
